a4be4615fdc0f49cb365ee710c52af7dfdfa5b24
Sushil Kumar <skgyan@gmail.com>
2018-10-15 11:14:49 +0530

    Playing with Subscriber class
    Playing with Subscriber class

--- 01-subscriber/index.js
+++ 01-subscriber/index.js
@@ -1,17 +1,24 @@
-import { from } from "rxjs"
+import { from, Subscriber } from "rxjs";
 
-const observable$ = from([1, 2, 3, 4, 5])
+const observable$ = from([1, 2, 3, 4, 5]);
 
 const subscriber = {
   next: value => {
-    console.log(value)
+    console.log(value);
   },
   complete: () => {
-    console.log("done")
+    console.log("done");
   },
   error: value => {
-    console.log(value)
+    console.log(value);
+  }
+};
+
+class TripleSubscriber extends Subscriber {
+  _next(value) {
+    console.log(value);
+    this.destination.next(value * 3);
   }
 }
 
-observable$.subscribe(subscriber)
+observable$.subscribe(new TripleSubscriber());
